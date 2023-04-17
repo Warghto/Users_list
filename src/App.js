bb64093235526db852636d4b77c75b871ca3f705
Tsaryk Vasyl <116073567+Warghto@users.noreply.github.com>
2023-04-17 07:32:43 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./styles.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
+
+// Тут список пользователей: https://reqres.in/api/users
+
+function App() {
+  return (
+    <div className="App">
+      <Users />
+      {/* <Success /> */}
+    </div>
+  );
+}
+
+export default App;
